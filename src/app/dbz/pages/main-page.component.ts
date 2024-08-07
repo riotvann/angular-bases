9b2335cc1a702b61c269b5c59c30cd58f7fae20f
Jorge Gámez <jgamezj@gmail.com>
2024-08-06 19:32:47 -0700

    lesson 62

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-dbz-main-page',
+    templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent {
+    constructor() { }
+
+}
